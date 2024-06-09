c12b47d6653e6fc5a3493af0645025ff280d9c19
myfmp <158035956+myfmp@users.noreply.github.com>
2024-06-10 00:29:03 +0100

    Update settings.py

--- FreeAnatomy/settings.py
+++ FreeAnatomy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!q6l8johe6%09e%z%b6ktblia#ulfj5ks(z-@1d!x6onimc16='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
